c0f92e4398d6fe2e982223ef2169c916fcde7d7f
Jeniffer Bitancort <jenifferbitancort@gmail.com>
2023-08-04 15:02:11 -0300

    Commit inicial

---
+++ Imprimir/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace _29___Imprimir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.buttonImprimir = new System.Windows.Forms.Button();
+            this.printPreviewControl1 = new System.Windows.Forms.PrintPreviewControl();
+            this.buttonVisualizar = new System.Windows.Forms.Button();
+            this.buttonAnterior = new System.Windows.Forms.Button();
+            this.buttonProx = new System.Windows.Forms.Button();
+            this.buttonVisualizPad = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.SuspendLayout();
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // buttonImprimir
+            // 
+            this.buttonImprimir.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonImprimir.Location = new System.Drawing.Point(644, 632);
+            this.buttonImprimir.Name = "buttonImprimir";
+            this.buttonImprimir.Size = new System.Drawing.Size(144, 47);
+            this.buttonImprimir.TabIndex = 0;
+            this.buttonImprimir.Text = "Imprimir";
+            this.buttonImprimir.UseVisualStyleBackColor = true;
+            this.buttonImprimir.Click += new System.EventHandler(this.buttonImprimir_Click);
+            // 
+            // printPreviewControl1
+            // 
+            this.printPreviewControl1.Location = new System.Drawing.Point(22, 23);
+            this.printPreviewControl1.Name = "printPreviewControl1";
+            this.printPreviewControl1.Size = new System.Drawing.Size(604, 655);
+            this.printPreviewControl1.TabIndex = 1;
+            // 
+            // buttonVisualizar
+            // 
+            this.buttonVisualizar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonVisualizar.Location = new System.Drawing.Point(644, 33);
+            this.buttonVisualizar.Name = "buttonVisualizar";
+            this.buttonVisualizar.Size = new System.Drawing.Size(144, 47);
+            this.buttonVisualizar.TabIndex = 2;
+            this.buttonVisualizar.Text = "Visualizar";
+            this.buttonVisualizar.UseVisualStyleBackColor = true;
+            this.buttonVisualizar.Click += new System.EventHandler(this.buttonVisualizar_Click);
+            // 
+            // buttonAnterior
+            // 
+            this.buttonAnterior.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAnterior.Location = new System.Drawing.Point(644, 101);
+            this.buttonAnterior.Name = "buttonAnterior";
+            this.buttonAnterior.Size = new System.Drawing.Size(71, 41);
+            this.buttonAnterior.TabIndex = 3;
+            this.buttonAnterior.Text = "Anterior";
+            this.buttonAnterior.UseVisualStyleBackColor = true;
+            this.buttonAnterior.Click += new System.EventHandler(this.buttonAnterior_Click);
+            // 
+            // buttonProx
+            // 
+            this.buttonProx.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonProx.Location = new System.Drawing.Point(721, 101);
+            this.buttonProx.Name = "buttonProx";
+            this.buttonProx.Size = new System.Drawing.Size(71, 41);
+            this.buttonProx.TabIndex = 4;
+            this.buttonProx.Text = "Próximo";
+            this.buttonProx.UseVisualStyleBackColor = true;
+            this.buttonProx.Click += new System.EventHandler(this.buttonProx_Click);
+            // 
+            // buttonVisualizPad
+            // 
+            this.buttonVisualizPad.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonVisualizPad.Location = new System.Drawing.Point(644, 579);
+            this.buttonVisualizPad.Name = "buttonVisualizPad";
+            this.buttonVisualizPad.Size = new System.Drawing.Size(144, 47);
+            this.buttonVisualizPad.TabIndex = 5;
+            this.buttonVisualizPad.Text = "Visualizar Impressão";
+            this.buttonVisualizPad.UseVisualStyleBackColor = true;
+            this.buttonVisualizPad.Click += new System.EventHandler(this.buttonVisualizPad_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 691);
+            this.Controls.Add(this.buttonVisualizPad);
+            this.Controls.Add(this.buttonProx);
+            this.Controls.Add(this.buttonAnterior);
+            this.Controls.Add(this.buttonVisualizar);
+            this.Controls.Add(this.printPreviewControl1);
+            this.Controls.Add(this.buttonImprimir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button buttonImprimir;
+        private System.Windows.Forms.PrintPreviewControl printPreviewControl1;
+        private System.Windows.Forms.Button buttonVisualizar;
+        private System.Windows.Forms.Button buttonAnterior;
+        private System.Windows.Forms.Button buttonProx;
+        private System.Windows.Forms.Button buttonVisualizPad;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
+
---
+++ Imprimir/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _29___Imprimir
+{
+    public partial class Form1 : Form
+    {
+
+        int x;
+        int y;
+        int largura;
+        int altura;
+        int num_linhas;
+        int pagina;
+        int num_paginas;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pagina = 0;
+            num_paginas = 0;
+        }
+
+        private void buttonImprimir_Click(object sender, EventArgs e)
+        {
+            largura = printDocument1.DefaultPageSettings.Bounds.Width;             
+            altura = printDocument1.DefaultPageSettings.Bounds.Height;              
+            x = 50;
+            y = 50;
+            num_linhas = 0;
+
+            printDocument1.Print();   
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+
+            //Título
+            string titulo = "Título da página";
+            Font letraTituloi = new Font("Verdana", 24, FontStyle.Bold, GraphicsUnit.Point);    
+            Brush pincelTitulo = new SolidBrush(Color.Red);                                   
+            Rectangle areaTitulo = new Rectangle(x + 50, y + 50, largura - 100, 100);          
+            //Formatação do título para centralizar
+            StringFormat formatoTitulo = new StringFormat();
+            formatoTitulo.Alignment = StringAlignment.Center;
+            formatoTitulo.LineAlignment = StringAlignment.Center;
+
+            //Texto
+            Font letra = new Font("Arial", 18, FontStyle.Bold, GraphicsUnit.Point);             
+            Brush pincel = new SolidBrush(Color.Black);                                         
+
+
+            List<string> linhas = new List<string>()
+            {
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+                "Alguns minutos de estudo por dia valem a pena",
+
+            };
+
+            while (num_linhas < linhas.Count)       
+            {
+                if (num_linhas == 0)   
+                {
+                    //Dados para impressão do título
+                    e.Graphics.DrawString(titulo, letraTituloi, pincelTitulo, areaTitulo);      
+                    y += 150;
+                }
+
+                //Dados para impressão do texto
+                e.Graphics.DrawString(linhas[num_linhas], letra, pincel, new Point(x, y));          
+                y += 30;
+                num_linhas++;
+
+                if (y >= altura - 50)
+                {
+                    //Mudar de página
+                    y = 50;
+                    e.HasMorePages = true;      
+                    num_paginas++;
+                    break;
+                }
+
+            }
+
+
+        }
+
+        private void buttonVisualizar_Click(object sender, EventArgs e)
+        {
+            largura = printDocument1.DefaultPageSettings.Bounds.Width;             
+            altura = printDocument1.DefaultPageSettings.Bounds.Height;              
+            x = 50;
+            y = 50;
+            num_linhas = 0;
+
+            printPreviewControl1.Document = printDocument1;
+        }
+
+        private void buttonAnterior_Click(object sender, EventArgs e)
+        {
+            if (pagina > 0)
+            {
+                printPreviewControl1.StartPage = --pagina;
+            }
+        }
+
+        private void buttonProx_Click(object sender, EventArgs e)
+        {
+            if (pagina < num_paginas)
+            {
+                printPreviewControl1.StartPage = ++pagina;
+            }
+
+        }
+
+        private void buttonVisualizPad_Click(object sender, EventArgs e)
+        {
+
+            largura = printDocument1.DefaultPageSettings.Bounds.Width;              
+            altura = printDocument1.DefaultPageSettings.Bounds.Height;              
+            x = 50;
+            y = 50;
+            num_linhas = 0; 
+            
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
+    }
+}
